12c6b4661f4085a316ae2d30c81708b5e31064ef
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-08-08 20:18:20 +0200

    prepare v0.1.0-alpha.0

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,11 @@
 import babel from "rollup-plugin-babel";
-import peerDepsExternal from "rollup-plugin-peer-deps-external";
-import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
-import localResolve from "rollup-plugin-local-resolve";
+import filesize from "rollup-plugin-filesize";
+import json from "rollup-plugin-json";
+import nodeBuiltins from "rollup-plugin-node-builtins";
+import nodeGlobals from "rollup-plugin-node-globals";
+import nodeResolve from "rollup-plugin-node-resolve";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
 
 export default {
   input: "src/Meditor.jsx",
@@ -21,10 +24,21 @@ export default {
   external: ["react", "react-dom"],
 
   plugins: [
+    // Externalize peer dependencies:
     peerDepsExternal(),
+    // Import JSON files:
+    json(),
+    // Transpile Babel to ES6:
     babel({ exclude: "./node_modules" }),
-    localResolve(),
-    resolve(),
-    commonjs()
+    // Convert CommonJS dependencies to ES2015:
+    commonjs(),
+    // Shim node.js globals:
+    nodeGlobals(),
+    // Shim node.js built-in libraries:
+    nodeBuiltins(),
+    // Locate dependencies via node.js resolution algorithm:
+    nodeResolve(),
+    // Calculate output bundle size:
+    filesize()
   ]
 };
