338ae0f2be58c521c8901af061ad3fa61bbc1f10
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-08-09 08:07:31 +0200

    scripts/ci/run_unit_tests: disable coveralls push

--- scripts/ci/run_unit_tests.sh
+++ scripts/ci/run_unit_tests.sh
@@ -6,4 +6,4 @@ set -e
 yarn --frozen-lockfile
 yarn test
 # Push tests coverage data to Coveralls:
-./node_modules/.bin/coveralls < ./coverage/lcov.info
+# ./node_modules/.bin/coveralls < ./coverage/lcov.info
