66d0be589a25faa30569ae6121377427d204d8b2
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-09-30 19:19:14 +0200

    feat: add typescript typings

---
+++ src/index.d.ts
@@ -0,0 +1,28 @@
+/// <reference types="react" />
+
+export type OnChangeOutput = {
+  html: string;
+  jsx: React.ReactElement;
+  source: string;
+  rawSource: string;
+};
+
+export type MeditorProps = Partial<{
+  className: string;
+  defaultValue: string;
+  disabled: boolean;
+  editorClassName: string;
+  editorStyle: object;
+  headersOffset: number;
+  noEditor: boolean;
+  noPreview: boolean;
+  noSpellCheck: boolean;
+  onChange: (output: OnChangeOutput) => void;
+  previewStyle: object;
+  previewClassName: string;
+  style: object;
+}>;
+
+declare class Meditor extends React.Component<MeditorProps> {}
+
+export = Meditor;
